e07b5c673a0986f1c37114f20aa4f6cd67e272b5
Rebeyka <Rebeyka@192.168.0.207>
2023-12-02 13:43:44 +0000

    Day 1 and 2 from 2023

--- adventofcode/src/main/java/adventofcode/solutions/year2023/Day1.java
+++ adventofcode/src/main/java/adventofcode/solutions/year2023/Day1.java
@@ -1,5 +1,15 @@
 package adventofcode.solutions.year2023;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import org.junit.Test;
+
 import adventofcode.utils.ChallengeDetails;
 import adventofcode.utils.ChallengeSolution;
 import adventofcode.utils.StringChallenge;
@@ -10,14 +20,78 @@ public class Day1 extends StringChallenge {
 	@Override
 	@ChallengeSolution
 	public String processA() {
-		return null;
+		return Integer.toString(input.stream().mapToInt(this::getCalibration).sum());
 	}
 
 	@Override
 	@ChallengeSolution
 	public String processB() {
-		// TODO Auto-generated method stub
-		return null;
+		return Integer.toString(input.stream().mapToInt(this::getRealCalibration).sum());
+	}
+	
+	private int getCalibration(String string) {
+		List<String> numbers = string.chars().filter(Character::isDigit).mapToObj(c -> Integer.toString(c - 48))
+				.collect(Collectors.toList());
+		return Integer.parseInt(numbers.get(0) + numbers.get(numbers.size() - 1));
 	}
 
+	
+	private int getRealCalibration(String string) {
+		List<String> numbers = new ArrayList<>();
+		for (int i=0; i<string.length(); i++) {
+			String digit = readDigit(string, i);
+			if (!digit.isBlank()) {
+				numbers.add(digit);
+			}
+		}
+		return Integer.parseInt(numbers.get(0) + numbers.get(numbers.size() - 1));
+	}
+	
+	private String readDigit(String string, int start) {
+		if (Character.isDigit(string.charAt(start))) {
+			return Character.toString(string.charAt(start));
+		}
+		if (string.substring(start).startsWith("one")) {
+			return "1";
+		}
+		if (string.substring(start).startsWith("two")) {
+			return "2";
+		}
+		if (string.substring(start).startsWith("three")) {
+			return "3";
+		}
+		if (string.substring(start).startsWith("four")) {
+			return "4";
+		}
+		if (string.substring(start).startsWith("five")) {
+			return "5";
+		}
+		if (string.substring(start).startsWith("six")) {
+			return "6";
+		}
+		if (string.substring(start).startsWith("seven")) {
+			return "7";
+		}
+		if (string.substring(start).startsWith("eight")) {
+			return "8";
+		}
+		if (string.substring(start).startsWith("nine")) {
+			return "9";
+		}
+		return "";
+	}
+
+	@Test
+	public void test() {
+		this.setInput("""
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+								""");
+		System.out.println(processB());
+	}
 }
---
+++ adventofcode/src/main/java/adventofcode/solutions/year2023/Day2.java
@@ -0,0 +1,102 @@
+package adventofcode.solutions.year2023;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import adventofcode.utils.AbstractChallenge;
+import adventofcode.utils.ChallengeDetails;
+import adventofcode.utils.ChallengeSolution;
+
+@ChallengeDetails(year = 2023, day = 2)
+public class Day2 extends AbstractChallenge<Day2.Game> {
+
+	@Override
+	@ChallengeSolution
+	public String processA() {
+		return Integer.toString(
+				getInput().stream().filter(g -> !validGame(g.getRounds(), 12, 13, 14)).mapToInt(Game::getId).sum());
+	}
+
+	@Override
+	@ChallengeSolution
+	public String processB() {
+		return Long.toString(getInput().stream().mapToLong(this::gamePower).reduce(Math::addExact).getAsLong());
+	}
+
+	private boolean validGame(List<Round> rounds, int red, int green, int blue) {
+		return rounds.stream()
+				.anyMatch(r -> (r.getCube().equals("green") && r.getAmount() > green)
+						|| (r.getCube().equals("red") && r.getAmount() > red)
+						|| (r.getCube().equals("blue") && r.getAmount() > blue));
+	}
+
+	public long gamePower(Game game) {
+		int red = game.getRounds().stream().filter(r -> r.getCube().equals("red")).mapToInt(r -> r.getAmount()).max().getAsInt();
+		int green = game.getRounds().stream().filter(r -> r.getCube().equals("green")).mapToInt(r -> r.getAmount()).max().getAsInt();
+		int blue = game.getRounds().stream().filter(r -> r.getCube().equals("blue")).mapToInt(r -> r.getAmount()).max().getAsInt();
+		return red * green * blue;
+	}
+	
+	@Override
+	public Game parse(String entry) {
+		String[] gameId = entry.split(":");
+		Game game = new Game(Integer.parseInt(gameId[0].replace("Game ", "")));
+		for (String round : gameId[1].split(";")) {
+			for (String cube : round.split(",")) {
+				String[] cubeInfo = cube.trim().split(" ");
+				game.getRounds().add(new Round(cubeInfo[1], Integer.parseInt(cubeInfo[0])));
+			}
+		}
+		return game;
+	}
+
+	@Test
+	public void test() {
+		this.setInput("""
+				Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+				Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+				Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+				Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+				Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+								""");
+		System.out.println(processA());
+	}
+
+	class Game {
+		private int id;
+		private List<Round> rounds;
+
+		public Game(int id) {
+			this.id = id;
+			this.rounds = new ArrayList<>();
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public List<Round> getRounds() {
+			return rounds;
+		}
+	}
+
+	class Round {
+		private String cube;
+		private int amount;
+
+		public Round(String cube, int amount) {
+			this.cube = cube;
+			this.amount = amount;
+		}
+
+		public String getCube() {
+			return cube;
+		}
+
+		public int getAmount() {
+			return amount;
+		}
+	}
+}
